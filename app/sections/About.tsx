e93166689eeccd8e82cffd3e1e569611cf6f8c81
zidanfadh <97351098+zidanfadh@users.noreply.github.com>
2024-12-31 04:28:13 +0700

    Update About.tsx

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
         >
             <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
                 <AnimatedTitle
-                    text={"A Little thing about me"}
+                    text={"A Little things about me"}
                     className={
                         "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
                     }
